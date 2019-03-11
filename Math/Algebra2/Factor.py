2023ca6b090099917ead8a71f62ceb7d1904fc42
nrhint <nathan@hintonclan.org>
2019-03-11 15:53:13 -0600

    fixed the factor.py by googlong slutions. Link in file for the source code.

--- Math/Algebra2/Factor.py
+++ Math/Algebra2/Factor.py
@@ -1,48 +1,27 @@
 ##Nathan Hinton
 ##This program will factor a number.
 ##I plan to use it for factoring radicals
+##See https://codereview.stackexchange.com/questions/144041/reduce-square-root-to-simplest-radical-form
+##For source/reference
+from math import sqrt
 
-from math import sqrt as sq
+def reduced_sqrt(n):
+    try:
+        root = int(sqrt(n))
+    except ValueError:
+        root = int(sqrt(n*-1))
+    for factor_root in range(root, 1, -1):
+        factor = factor_root * factor_root
+        if n % factor == 0:
+            reduced = n // factor
+            return (factor_root, reduced)
+    return (1, n)
 
-def factor(num):
-    if num > 9999999999999:
-        print("Number To large and may take a while to do.")
-        return 0
-    factors = []
-    lastFact = 1
-    for x in range(1, int(num/2)+1):
-        if num%x == 0:
-            if lastFact == num/x:
-                break
-            elif x == num/x:
-                factors.append(x)
-                break
-            #print(x, num/x)
-            factors.append(x)
-            factors.append(int(num/x))
-            lastFact = x
-    return factors
-
-def findCommon(i):
-    num = 1
-    for x in i:
-        num*x
-    f = factor(num)
-    for x in f:
-        for n in i:
-            if x%x == 0
-                print(x)
-
-def findSquares(num):
-    facts = factor(num)
-    facts.sort()
-    print(facts)
-    end = len(facts)-1
-    m = sq(facts[end])+1
-    #3print(m)
-    for x in range(0, int(m)):
-        if x == 1:
-            pass
-        elif x**2 in facts:
-            print(x)
-    
+def print_reduced_sqrt(n):
+    coefficient, reduced = reduced_sqrt(n)
+    if coefficient == 1:
+        print('\u221A', reduced)
+    elif reduced == 1:
+        print(coefficient)
+    else:
+        print(coefficient, '\u221A', reduced)
