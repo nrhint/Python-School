63267fb530954f56b263cd9b34f87daa4d7f6c71
nrhint <nathan@hintonclan.org>
2019-03-12 11:26:03 -0600

    added GCM to alg2 Factor.py

--- Math/Algebra2/Factor.py
+++ Math/Algebra2/Factor.py
@@ -30,12 +30,33 @@ def print_reduced_sqrt(n):
         print(coefficient, '\u221A', reduced)
 
 def factor(n):
-    count = 0;
+    count = 0
+    facts = []
     number = int(n)
-    for i in range(2, number-1):
+    for i in range(1, round(sqrt(number)+1)):
         if number%i == 0:
-            print(i)
+            #print('%i : %i'%(i, number/i))
+            facts.append(i)
+            facts.append(int(number/i))
             i += 1
             count += 1
     if count==0:
-        print(number,"is a prime number.")
+        #print(number,"is a prime number.")
+        facts.append(1)
+        facts.append(number)
+    return facts
+
+def printFactor(n):
+    print(factor(n))
+
+def greatestCommonFactor(l):
+    allFacts = []
+    for f in l:
+        allFacts.append(factor(f))
+    for x0 in allFacts[0]:
+        true = 0
+        for s in allFacts:
+            if x0 in s:
+                true +=1
+        if true == len(allFacts):
+            print(x0)
