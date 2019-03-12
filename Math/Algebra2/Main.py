d277ee198b74bb243c346e53ec5941a62a4aecc5
nrhint <nathan@hintonclan.org>
2019-03-11 19:56:16 -0600

    Alg2 main now can do positive trinomials. working on negative

--- Math/Algebra2/Main.py
+++ Math/Algebra2/Main.py
@@ -5,13 +5,42 @@ print("Setting up...")
 import Factor
 import takeInput
 
+def calcTri(a, b):
+    x1 = 0
+    x2 = 0
+    while x1 < a:
+        x1 +=1
+        x2 = 0
+        #print('x1:%s'%+x1)
+        while x2 < a:
+            x2 +=1
+            #print('x2:%s'%+x2)
+            if x1+x2 == a and x1*x2 == b:
+                return(x1, x2)
+
 def trinomial():
     #x**2+(a+b)x+ab
+    #Forms:
+    #x**2+7x+12
+    #(x+a)(x+b)
     i = takeInput.rawInput()
     seperate = i.split('+')
     print(seperate)
+    #print(seperate[1])
+    for x in range(len(seperate[1])):
+        if x == '-':
+            poly1 = int(seperate[1][0:1])
+        else:
+            poly1 = int(seperate[1])
+    for x in range(len(seperate[2])):
+        if x == '-':
+            poly2 = int(seperate[2][0:1])
+        else:
+            poly2 = int(seperate[2])
+    a, b = calcTri(poly1, poly2)
+    print(a, b)
+    print('(x+%i)(x+%i)'%(a, b))
     return seperate
-
 
 #Main loop:
 mainLoop = True
--- Math/Algebra2/takeInput.py
+++ Math/Algebra2/takeInput.py
@@ -5,10 +5,23 @@
 print("Initalizing takeInput.py...")
 print("Does not handle lists. only strings")
 
-def takeInput():
+def takeUserInput():
     #Take input
     print("Input:")
     i = input()
+    dat = []
+
+    #Process input
+    for char in i:
+        try:
+            dat.append(int(char))
+        except ValueError:
+            dat.append(str(char))
+    #return the list of values as ints and chars.
+    return dat
+
+def takeInput(i):
+    #Take input
     dat = []
 
     #Process input
