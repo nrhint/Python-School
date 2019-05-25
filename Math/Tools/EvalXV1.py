4553a4c934fa25a4a6964269d0c7dd3af13eac8c
nrhint <nathan@hintonclan.org>
2019-05-24 22:32:52 -0600

    it works to move the x around if there is multuplication. working on subtraction

--- Math/Tools/EvalXV1.py
+++ Math/Tools/EvalXV1.py
@@ -4,7 +4,14 @@
 def i():
     print("Enter the equation:")
     return str(input())
-def FindTheX(i):
+def removeSpaces(i):#Remove spaces from input so that long equations take less time.
+    i2 = i.split()
+    i3 = ''
+    for x in i2:
+        i3 += x
+    return i3
+def FindTheX(i):#This will find x.
+    i = removeSpaces(i)
     running = True
     state = 'init'
     length = len(i)
@@ -22,8 +29,78 @@ def FindTheX(i):
                     xpos.append(pos)
                 pos += 1
     return i, xpos
+def moveX(i):
+    running = True
+    state = 'init'
+    output = i
+    #Find which side x is on:
+    left = i.split('=')[0]
+    right = i.split('=')[1]
+    if 'x' in left:
+        side = 'left'
+    else:
+        side = 'right'
+    while running == True:
+        if state == 'init':
+            print("Starting move x...")
+            state = 'multDiv'
+        elif state == 'test':
+            if '*' in output or '/' in output:
+                state = 'multDiv'
+            elif '+' in output or '-' in output:
+                state = 'addSub'
+            else:
+                state = 'end'
+        elif state == 'multDiv':
+            temp = i
+        #try:
+            splt = temp.split('x')#Find a place where x is.
+            xexpr = splt[0]
+            #Check for multuplication:
+            if '*' in xexpr:
+                print('mult')
+                l = len(xexpr)
+                loop = 1
+                end = 0
+                for x in range(-l+2, 1):
+##                    print("LOOPING")
+##                    print(x)
+                    num = ''
+                    item = xexpr[-x]
+                    #print(item)
+                    if item == '*':
+                        end = -x
+                    elif item == '/':
+                        end = -x
+                    elif item == '+':
+                        end = -x
+                    elif item == '-':
+                        end = -x
+                    else:
+##                        print(item)
+##                        print(loop)
+                        num +=str(int(item)*10**(loop-1))
+##                        print(num)
+                    loop += 1
+                if side == 'left':
+                    tmpExpr = right+'/'+str(num)
+                    output = str(i[0:end])+'x='+tmpExpr
+                    print('left')
+                else:
+                    tmpExpr = left+'/'+str(num)
+            #print(tmpExpr)
+            print(output)
+            state = 'end'
+        elif state == 'addSub':
+            pass
+        elif state == 'end':
+            print("Finished")
+            return output
 def testV2():#Goal: make it so that you can move the x around
-    pass
+    print("Test V2")
+    rawEquation = i()
+    equation, xPos = FindTheX(rawEquation)
+    print(equation, xPos)
 #####################################################
 def testV1(rangeMin = -1, rangeMax = 2):
     print("TEST V1")
@@ -39,4 +116,6 @@ def testV1(rangeMin = -1, rangeMax = 2):
 
 
 ##Here is the program being tested
-testV1()
+#testV2()
+i = '5*x=32'
+moveX(i)
