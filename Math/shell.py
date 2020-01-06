b43b1f8c76fe67ccceef47e69d929f16a2a4b726
nrhint <nathan@hintonclan.org>
2020-01-06 08:28:37 -0700

    uploading notes...

---
+++ Math/shell.py
@@ -0,0 +1,52 @@
+Python 3.6.9 (default, Nov  7 2019, 10:44:02) 
+[GCC 8.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> def sqrt(x):
+	return round(math.sqrt(x), 2)
+
+>>> def solve(x, y):
+	return (round(y+sqrt(x), 2), round(y-sqrt(x), 2))
+
+>>> solve(3, 8)
+(9.73, 6.27)
+>>> math.factorial(5)
+120
+>>> math.factorial(5)
+KeyboardInterrupt
+>>> 1+2+3+4+5
+15
+>>> 5/2
+2.5
+>>> 5*2+5
+15
+>>> 4150*2+4150
+12450
+>>> 12450*1600
+19920000
+>>> 4160*1600
+6656000
+>>> 6*2
+12
+>>> 12+6
+18
+>>> 1+2+3+4+5+6
+21
+>>> num = 0
+>>> for x in range(1, 4300):
+	num += x
+
+	
+>>> num
+9242850
+>>> 9242850*1600
+14788560000
+>>> 14788560000
+KeyboardInterrupt
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
